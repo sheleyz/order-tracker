f9045badd07d70d20ae7c3c6f4bc46a7c3645137
sheleyz <59847602+sheleyz@users.noreply.github.com>
2023-03-10 13:11:51 -0500

    Code cleanup and initial setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders order tracker heading", () => {
+    render(<App />);
+    const headingElement = screen.getByText(/Order Tracker/i);
+    expect(headingElement).toBeInTheDocument();
 });
