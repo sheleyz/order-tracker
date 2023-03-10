14baa4793aa9135a2ed08640d8ea36ce6511f4bf
sheleyz <59847602+sheleyz@users.noreply.github.com>
2023-03-10 16:37:07 -0500

    Added header logo, updated manifest name

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import "./index.css";
 // Components
 import DataTable from "./components/OrdersTable";
 
+// Images
+import REDTechnologiesLogo from "./images/red-technologies-logo.png";
+
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Order Tracker</h1>
+                <img src={REDTechnologiesLogo} alt="RED Technologies logo" />
+                <h1>Home</h1>
             </header>
             <DataTable />
         </div>
