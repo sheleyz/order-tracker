a293e3ab85b91843af04d4dd8dfcaea9bc8cd468
sheleyz <59847602+sheleyz@users.noreply.github.com>
2023-03-13 12:04:11 -0400

    Updated tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,12 @@ import App from "./App";
 
 test("renders order tracker heading", () => {
     render(<App />);
-    const headingElement = screen.getByText(/Order Tracker/i);
+    const headingElement = screen.getByText(/Home/i);
     expect(headingElement).toBeInTheDocument();
+});
+
+test("renders OrdersGrid", () => {
+    render(<App />);
+    const gridElement = screen.getByText(/Order ID/i);
+    expect(gridElement).toBeInTheDocument();
 });
